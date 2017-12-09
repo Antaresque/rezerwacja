03409a4b7030343f40382e6fc931bc99487d86a6
Adam Bytniewski <adijedendwa@gmail.com>
2017-12-09 17:58:21 +0100

    importy do routing module + jakieś duperele

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -5,7 +5,10 @@ import { HomeComponent } from './home/home.component';
 import { AuthComponent } from './auth/auth.component';
   import { RegisterComponent } from './auth/register/register.component';
   import { LoginComponent } from './auth/login/login.component';
-
+import { PokojComponent } from './pokoj/pokoj.component';
+import { PracComponent } from './prac/prac.component';
+import { UserComponent } from './user/user.component';
+import { SzefComponent } from './szef/szef.component';
 import { PageNotFoundComponent } from './_public/pagenotfound/pagenotfound.component';
 
 const appRoutes: Routes = [
@@ -15,6 +18,10 @@ const appRoutes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent }
   ]},
+  { path: 'pokoj', component: PokojComponent },
+  { path: 'prac', component: PracComponent },
+  { path: 'user', component: UserComponent },
+  {path: 'szef', component: SzefComponent },
   { path: '**', component: PageNotFoundComponent } // 404
 ];
 
