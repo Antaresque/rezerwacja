f4a2117a3aeaa39707582802c333a887da254eca
Antaresque <olekfer@yahoo.pl>
2017-12-11 01:50:54 +0100

    maile moze zadziala nwm

--- php/_mail.php
+++ php/_mail.php
@@ -1,9 +1,37 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 function mail_message($email, $type){
-  $mail = 'admin@hotel-jajusz.cba.pl';
+  require_once '_mail/Exception.php';
+  require_once '_mail/PHPMailer.php';
 
-  $headers =  'MIME-Version: 1.0'."\r\n";
-  $headers .= 'From: '.$mail."\r\n";
-  $headers .= 'Content-type: text/html; charset=utf-8'."\r\n";
+  $mail = new PHPMailer;
 
+  $mail->isSMTP();                                      // Set mailer to use SMTP
+  $mail->Host = 'mail.cba.pl';                          // Specify main and backup SMTP servers
+  $mail->SMTPAuth = true;                               // Enable SMTP authentication
+  $mail->Username = 'admin@hotel-jajusz.cba.pl';        // SMTP username
+  $mail->Password = 'Janusz1';                          // SMTP password
+  $mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
+
+  $mail->From = 'admin@hotel-jajusz.cba.pl';
+  $mail->FromName = 'Hotel Jajusz';
+  $mail->addAddress($email);                            // Add a recipient
+
+  //$mail->WordWrap = 50;                               // Set word wrap to 50 characters
+  $mail->isHTML(true);                                  // Set email format to HTML
+
+  if($type = "VERIFY"){
+    $mail->Subject = 'Potwierdzenie konta';
+    $mail->Body    = 'testowy mail';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+  }
+
+  if(!$mail->send()) {
+      echo 'Message could not be sent.';
+      echo 'Mailer Error: ' . $mail->ErrorInfo;
+  } else {
+      echo 'Message has been sent';
+  }
 }
--- php/api.php
+++ php/api.php
@@ -3,7 +3,10 @@ require_once('_host.php');
 require_once('_hash.php');
 require_once('_messages.php');
 require_once('_JWT.php');
+require_once('_mail.php');
 use \Firebase\JWT\JWT;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 //check for localhost (OPTIONS pre-flight response)
 $method = $_SERVER['REQUEST_METHOD'];
